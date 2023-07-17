a59bb93ccc6e8bae22280b1ed595c6a64262bea0
Chandan Kumar Behera <118505620+chandankumar1425@users.noreply.github.com>
2023-07-17 15:40:36 +0530

    github

---
+++ script/dashboard.js
@@ -0,0 +1,42 @@
+const mainContsiner = document.getElementById("main_container");
+const BaseServerUrl = `https://quizapp-rbes.onrender.com`
+fetchAndRenderData();
+function fetchAndRenderData(){
+    fetch(`${BaseServerUrl}/quiz/allquiz`)
+    .then(res=>res.json())
+    .then((data)=>{
+        console.log(data);
+
+        renderDom(data);
+    })
+}
+function renderDom(arr=[]){
+    mainContsiner.innerHTML=null;
+    arr.forEach((el)=>{
+        const card = document.createElement("div");
+        const title = document.createElement("h3");
+        title.innerText = el.title;
+        const des =  document.createElement("p");
+        des.innerText=el.description
+        const noOfQues =  document.createElement("p");
+        noOfQues.innerText = `Questions : ${el.questions.length}`
+        const creator =  document.createElement("p");
+        creator.innerText=`Created By :- ${el.creator}`
+        const takeQuiz =  document.createElement("button"); 
+        takeQuiz.innerText = 'Take Quiz'
+        takeQuiz.addEventListener("click",()=>{
+            localStorage.setItem("quizId",el._id);
+            setTimeout(()=>{
+                window.location.href="./quiz.html"
+            },2000)  
+        })
+        const leaderboard =  document.createElement("button");
+        leaderboard.innerText="Leaderboard"
+        leaderboard.addEventListener('click',()=>{
+            window.location.href="./leaderboard.html"
+        })
+        card.append(title,des,creator,noOfQues,takeQuiz,leaderboard);
+        mainContsiner.append(card)
+    })
+   
+}
